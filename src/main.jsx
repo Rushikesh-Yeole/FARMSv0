591cd15a7acce4c083a92d3ed4419efc2f85f0ae
kalpeshshirsath11 <kalpeshshirsath11@gmail.com>
2025-02-09 07:40:01 +0530

    hashrouting

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HashRouter>  {/* Wrap your App with HashRouter */}
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
   </StrictMode>
 );
